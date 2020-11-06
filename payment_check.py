c2156da2e943f17383e1f6d3db36ff0da05bfb90
willking92 <74033670+willking92@users.noreply.github.com>
2020-11-06 11:24:27 +0800

    Create payment_check.py

---
+++ payment_check.py
@@ -0,0 +1,24 @@
+import openpyxl
+from openpyxl import load_workbook
+keywordlist = ['山语墅','西区供水','undefined']
+class projectinfo:
+    name='undefined'
+    def _init_(self,serialnum,price):
+        self.serialnum = serialnum
+        self.price = price
+    def keywordcheck(selfname):
+        for i in keywordlist:
+            if selfname.find(i):
+                self.name=i
+    collectioninfo = 0
+    pass
+
+if __name__ == '__main__':
+    #read xlsx file as exported and collect basic datas for future use
+    wb =load_workbook(filename = 'D:/pt/registrationexport.xlsx')
+    wb.active
+    ws = wb.get_sheet_by_name('Sheet1')
+    for i in range(3,ws.max_row):
+        proj = projectinfo(ws.cell(i,2).value,ws.cell(i,15).value)
+        proj.keywordcheck(ws.cell(i,13).value)
+        
